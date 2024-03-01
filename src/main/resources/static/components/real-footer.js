2cb37c95a9dd7486b40057623c4be09eaba1564d
lodny <drinkjuice@naver.com>
2024-03-02 08:36:20 +0900

    real-footer.js

--- src/main/resources/static/components/real-footer.js
+++ src/main/resources/static/components/real-footer.js
@@ -1,72 +1,67 @@
-class RealFooter extends HTMLElement {
+const style = `<style>
+    a {
+        color: #5CB85C;
+    }
 
+    footer {
+        position: fixed;
+        bottom: 0;
+        z-index: 999;
+        width: 100%;
+        
+        font-family: "Titillium Web", sans-serif;
+        background-color: #F3F3F3;
+        
+        margin-top: 2rem;
+    }
+    
+    .container {
+        line-height: 1.5rem;
+        padding: 1rem 5rem;
+        
+        display: flex;
+    }
+    
+    .logo-font {
+        font-size: 1rem;
+    }
+    
+    .attribution {
+        color: #aaa;
+        font-size: 0.7rem;
+        font-weight: lighter;
+        margin-left: 1rem;
+    }           
+</style>`;
+
+
+function getTemplate() {
+    return `
+        <link rel="stylesheet" href="../css/common.css">
+        ${style}
+        
+        <footer>
+            <div class="container">
+                <a href="/" class="logo-font">conduit</a>
+                <span class="attribution">
+                      An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp;
+                      design licensed under MIT.
+                </span>
+            </div>
+        </footer>
+    `;
+}
+
+class RealFooter extends HTMLElement {
     constructor() {
         super();
-
-        this.shadow = this.attachShadow({mode: 'open'});
+        this.attachShadow({mode: 'open'});
+        this.shadow.innerHTML = getTemplate()
     }
 
     connectedCallback() {
-        this.render();
-    }
-
-    render() {
-        this.shadow.innerHTML = `
-            ${style()}
-            
-            <footer>
-                <div class="container">
-                    <a href="/" class="logo-font">conduit</a>
-                    <span class="attribution">
-                          An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp;
-                          design licensed under MIT.
-                    </span>
-                </div>
-            </footer>
-        `
+        console.log('real-footer::connectedCallback(): 1:', 1);
     }
 }
 
 customElements.define('real-footer', RealFooter);
-
-function style() {
-    return `
-        <link rel="stylesheet" href="../css/common.css">
-
-        <style>
-            a {
-                color: #5CB85C;
-            }
-    
-            footer {
-                position: fixed;
-                bottom: 0;
-                z-index: 999;
-                width: 100%;
-                
-                font-family: "Titillium Web", sans-serif;
-                background-color: #F3F3F3;
-                
-                margin-top: 2rem;
-            }
-            
-            .container {
-                line-height: 1.5rem;
-                padding: 1rem 5rem;
-                
-                display: flex;
-            }
-            
-            .logo-font {
-                font-size: 1rem;
-            }
-            
-            .attribution {
-                color: #aaa;
-                font-size: 0.7rem;
-                font-weight: lighter;
-                margin-left: 1rem;
-            }
-                    
-        </style>`;
-}
